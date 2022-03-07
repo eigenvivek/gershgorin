9f47db371962212ac566d65131d3af9602dd0f5f
v715 <vgopala4@jhu.edu>
2022-03-07 15:09:06 -0500

    Add implementation of Gershgorin's disc

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# Get the version number from the version.py file
+with open("gershgorin/version.py") as f:
+    __version__ = f.read().split()[-1].strip("'")
+
+
+# Get the requirements from the requirements.txt file
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="gershgorin",
+    version=__version__,
+    description="Visualize the Gershgorin discs that bound the spectrum of a square matrix.",
+    url="https://github.com/v715/gershgorin",
+    author="Vivek Gopalakrishnan",
+    author_email="vivekg@mit.edu",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
