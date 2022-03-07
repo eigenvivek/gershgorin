01d59d66ca6f15183e87a3334866aa52414cdb8b
v715 <vgopala4@jhu.edu>
2022-03-07 15:29:09 -0500

    Move required packges into setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup, find_packages
 
+
 # Get the version number from the version.py file
 with open("gershgorin/version.py") as f:
     __version__ = f.read().split()[-1].strip("'")
-
-
-# Get the requirements from the requirements.txt file
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
 
 setup(
     name="gershgorin",
@@ -16,8 +12,9 @@ setup(
     url="https://github.com/v715/gershgorin",
     author="Vivek Gopalakrishnan",
     author_email="vivekg@mit.edu",
+    license="MIT",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=["matplotlib", "numpy", "adjustText"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
